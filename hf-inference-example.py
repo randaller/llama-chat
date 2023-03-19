a80a6066853e3f9615c7bba6fbe8d6e3a7534892
randaller <randaller@users.noreply.github.com>
2023-03-19 12:50:59 +0300

    Update hf-inference-example.py

--- hf-inference-example.py
+++ hf-inference-example.py
@@ -5,9 +5,11 @@ MODEL = 'decapoda-research/llama-7b-hf'
 # MODEL = 'decapoda-research/llama-30b-hf'
 # MODEL = 'decapoda-research/llama-65b-hf'
 
+# MODEL = './trained'
+
 tokenizer = llamahf.LLaMATokenizer.from_pretrained(MODEL)
 model = llamahf.LLaMAForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage=True)
 model.to('cpu')
 
 batch = tokenizer("The highest mountain in China is ", return_tensors="pt")
-print(tokenizer.decode(model.generate(batch["input_ids"].cpu(), max_length=256)[0]))
+print(tokenizer.decode(model.generate(batch["input_ids"].cpu(), max_length=100)[0]))
